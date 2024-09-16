1af3b9009394f14dbf4044ab94e5bb66a7b50d51
Universe1609 <davidrolandoaguirre@gmail.com>
2024-09-16 09:59:07 -0500

    astro.config dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Universe1609.github.io',
+    site: 'http://www.devopsuniverse.cloud,
 })
